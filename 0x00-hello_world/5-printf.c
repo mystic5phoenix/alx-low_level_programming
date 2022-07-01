7d6760e6b86d6c43bb2558710e2e1d2dc01bf705
mystic5phoenix <bekeletigist618@gmail.com>
2022-07-01 03:42:28 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,13 @@
 
 /*
  *main - Entry point
- *printing strings
+ *printing using printf function
  *Return: Always 0 (Sucess)
  */
 int main(void)
 
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
 return (0);
 }
