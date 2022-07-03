b74ee04fe108f1d9948e07b9f6a31886221b4eb7
mystic5phoenix <bekeletigist618@gmail.com>
2022-07-03 11:53:46 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *printing alphabet in lowercase
+ *Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int n;
+
+for (n = 'a'; n <= 'z'; n++)
+
+putchar(n);
+
+putchar('\n');
+
+return (0);
+
+}
