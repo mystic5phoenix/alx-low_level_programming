0df9785a757da628270c4e7297ce633972f7cee2
mystic5phoenix <bekeletigist618@gmail.com>
2022-07-03 12:36:56 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 
 /**
  *main - Entry point
- *prints all single digit numbers of base 10 starting from 0, followed by a new line
+ *prints all single digit numbers of base 10 starting from 0
  *Return: Always 0 (Success)
  */
 int main(void)
