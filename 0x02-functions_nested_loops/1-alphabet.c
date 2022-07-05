c4cdf59e5012e888014c5f9092fafa38b8e76e56
mystic5phoenix <bekeletigist618@gmail.com>
2022-07-05 02:56:46 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void print_alphabet(void)
+
+{                                                                                                                        
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+printf("%c", c);
+}
+printf("\n");
+}
