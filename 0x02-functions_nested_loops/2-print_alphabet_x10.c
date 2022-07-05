1fdf1bbfd57250db7089aef1f5807fe0539ab6d3
mystic5phoenix <bekeletigist618@gmail.com>
2022-07-05 05:01:16 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints alphabet 10 times
+ *nested while loop
+ */
+void print_alphabet_x10(void)
+
+{
+int i=0;
+char c;
+while (i < 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+printf("%c", c);
+}
+printf("\n");
+i++;
+}
+}
