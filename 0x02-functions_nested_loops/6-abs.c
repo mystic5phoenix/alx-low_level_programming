097298c794cff971e391d79044284db1d33205b2
mystic5phoenix <bekeletigist618@gmail.com>
2022-07-05 09:01:10 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,22 +3,22 @@
 
 /**
  *_abs - a function that computes abs
- *prints the abs value
+ *@n: the integer
  *Return: 0
  */
 int _abs(int n)
 {
 if (n > 0)
 {
-return n;
+return (n);
 }
-else if (n ==0)
+else if (n == 0)
 {
-return n;
+return (n);
 }
 else
 {
 n = (-1 * n);
-return n;
+return (n);
 }
 }
