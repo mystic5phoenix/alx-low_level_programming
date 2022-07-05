3470d7ec90041184a04219f80b1649279ec20574
mystic5phoenix <bekeletigist618@gmail.com>
2022-07-05 03:38:15 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
+ *Print_alphabet is a user defined function
  *prints alphabets
  */
 void print_alphabet(void)
-{                                                                                                                      
+{                                                                            
 char c;
 for (c = 'a'; c <= 'z'; c++)
 {
