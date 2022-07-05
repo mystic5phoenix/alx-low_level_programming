e385a0f821bc11422f48d785f394ff37e7bdfe7e
mystic5phoenix <bekeletigist618@gmail.com>
2022-07-05 03:31:38 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
+#include "main.h"
 void print_alphabet(void)
-
 {                                                                                                                        
 char c;
 for (c = 'a'; c <= 'z'; c++)
