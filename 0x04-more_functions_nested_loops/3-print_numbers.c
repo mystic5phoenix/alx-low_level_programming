9603479c44d5ae0b2ab39d0a247eb760f0004b3a
mystic5phoenix <bekeletigist618@gmail.com>
2022-07-08 09:24:58 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_numbers - prints the numbers
+ *Return: void
+ */
+void print_numbers(void)
+
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
